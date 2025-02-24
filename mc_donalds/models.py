5280d2d01968305f95e1c1bd6b3aa54910059bc1
Aleksey <arekabu@yandex.com>
2025-02-24 15:36:28 +0300

    Models edited

--- mc_donalds/models.py
+++ mc_donalds/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    pass
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+
+class Staff(models.Model):
+    director = 'DI'
+    admin = 'AD'
+    cook = 'CO'
+    cashier = 'CA'
+    cleaner = 'CL'
+
+    POSITIONS = [
+        (director, 'Директор'),
+        (admin, 'Администратор'),
+        (cook, 'Повар'),
+        (cashier, 'Кассир'),
+        (cleaner, 'Уборщик')
+    ]
+    full_name = models.CharField(max_length=255)
+    position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
+    labor_contract = models.IntegerField()
+
+class ProductOrder(models.Model):
+    pass
+
+
