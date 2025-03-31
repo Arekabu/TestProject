bddc62592377a1ad94d718d2a6354f61675b2ee5
Aleksey <arekabu@yandex.com>
2025-03-31 12:04:13 +0300

    Module_22_Unit_03_all_done

---
+++ TestProject/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestProject.settings')
+
+app = Celery('TestProject')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
--- mc_donalds/apps.py
+++ mc_donalds/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+import redis
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class McDonaldsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mc_donalds'
+
+red = redis.Redis(
+    host='redis-17520.c328.europe-west3-1.gce.redns.redis-cloud.com',
+    port='17520',
+    password=os.getenv('REDIS_PASSWORD')
+)
